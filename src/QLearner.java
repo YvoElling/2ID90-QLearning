cd0f75360dbff389ab63c42e5317e64d6e1d141f
Yvo Elling <yvo.elling@solarteameindhoven.nl>
2020-03-26 11:56:44 +0100

    Implemented basic Q initialization

--- src/QLearner.java
+++ src/QLearner.java
@@ -1,3 +1,7 @@
+
+import java.util.Random;
+
+//nl.tue.s2id90.qlearning.peach.QInputReader
 public class QLearner {  
     /**
      * This method performs Q-learning. TODO complete documentation.
@@ -7,20 +11,42 @@ public class QLearner {
      * @param noIterations
      * @return 
      */
-    public String execute(Integer[][] rewards, Integer[][] paths, Double gamma, Integer noIterations)
+    public String execute(Integer[][] rewards, Integer[][] paths, Double gamma,
+            Integer noIterations)
     {
-        final Double[][] Q = null;
+        //Create the Q(uality)-table
+        Double[][] Q = new Double[rewards.length][rewards[0].length];
         
-        // InitializeQ
+        //Initialize the Q-table for all pairs(state, action) to 0.0
+        //Rewards is of shape (s, a), so we use this to create the Q_value of 
+        //the correct size
+        for(int row = 0; row < rewards.length; ++row) {
+            for( int column = 0; column < rewards[row].length; ++column) {
+                Q[row][column] = 0.0;
+            }
+        }
         
+        //Repeat the process of 
+            int state = new Random().nextInt(paths.length);
+            Integer[] actions = paths[state];
+            this.execute(Q, rewards, actions, gamma);     
         // Do Q-learning
-        
+       
         return policy(Q);
     }
     
-    /** do Q-learning for one path.  TODO complete documentation. */
-    private void execute(Double[][] Q, Integer[][] rewards, Integer[] path, Double gamma) {
+    private Integer[] selectRandomState(Integer[][] paths){
+        int rnd = 
+        return paths[rnd];
+    }
     
+    /** do Q-learning for one path.  TODO complete documentation. */
+    private void execute(Double[][] Q, Integer[][] rewards, Integer[] path, 
+            Double gamma) {
+        for (Integer action : path) {
+            Integer newState = action;
+            
+       }
     }
 
     /**
